5825b0aeb21d03f7bbadac78e675eb61c826af12
Kandreas9 <andkarido@gmail.com>
2021-07-10 20:33:02 +0300

    style(index.tsx): Styled the content on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "../style/home/home.css";
 
 export default function Home() {
 	return (
-		<div className="page">
-			<p>this is cool</p>
-		</div>
+		<section className="hero">
+			<section className="hero-content">
+				<div className="hero-logo">
+					<img src="/deno.svg" alt="deno logo" />
+				</div>
+
+				<h1>Deno 101</h1>
+				<p>Last Updated:</p>
+				<p>10/7/2021</p>
+			</section>
+		</section>
 	);
 }
